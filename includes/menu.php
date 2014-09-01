11543190d34accc8af0b2f2999f5413347be1cf6
Hrprofilergpec2014 <hr.profiler.gpec@gmail.com>
2014-09-01 22:22:53 +0200

    Revert "Ajout de la version 1 du site HR PROFILER"
    This reverts commit c04e9d80cd8351b3f06ad200802d1dd8b8e565a8.

--- includes/menu.php
+++
@@ -1,9 +0,0 @@
-<div id="menu_entete">
-<ul class="nav nav-tabs navbar-inverse">
-	<li><a href="//hr-profiler.fr/pages/p_cartographie.php">Cartographie</a></li>
-    <li><a href="//hr-profiler.fr/pages/p_edition.php">Edition</a></li>
-    <li><a href="//hr-profiler.fr/pages/p_config_gestion.php">Configuration </a></li>
-    <li><a href="//hr-profiler.fr/pages/p_import_export.php">Import/Export</a></li>
-</ul>
-</div>
- 
